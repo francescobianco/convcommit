d4e8459167f243f9d8bb6eaeada882b0fe2cb978
francesco <info.francescobianco@gmail.com>
2025-02-10 18:07:08 +0100

    Release

--- src/main.sh
+++ src/main.sh
@@ -38,14 +38,30 @@ main() {
   convcommit_file=".convcommit"
 
   if [ ! -f "${convcommit_file}" ]; then
-    echo "type: feat" >> "${convcommit_file}"
+    # shellcheck disable=SC2129
+    echo "type:fix" >> "${convcommit_file}"
+    echo "type:build" >> "${convcommit_file}"
+    echo "type:chore" >> "${convcommit_file}"
+    echo "type:docs" >> "${convcommit_file}"
+    echo "type:merge" >> "${convcommit_file}"
+    echo "type:feat" >> "${convcommit_file}"
+    echo "type:style" >> "${convcommit_file}"
+    echo "type:refactor" >> "${convcommit_file}"
+    echo "type:perf" >> "${convcommit_file}"
+    echo "type:test" >> "${convcommit_file}"
+    echo "type:ci" >> "${convcommit_file}"
+    echo "type:revert" >> "${convcommit_file}"
+    echo "type:security" >> "${convcommit_file}"
+    echo "type:deps" >> "${convcommit_file}"
+    echo "type:wip" >> "${convcommit_file}"
+    echo "type:init" >> "${convcommit_file}"
+    echo "scope:_" >> "${convcommit_file}"
+    echo "message:_" >> "${convcommit_file}"
   fi
 
-  commit_type=$(convcommit_selector "$convcommit_file" "type")
+  commit_type=$(convcommit_selector "$convcommit_file" "type" 4 10)
   commit_scope=$(convcommit_selector "$convcommit_file" "scope")
   commit_message=$(convcommit_selector "$convcommit_file" "message")
-
-  echo "Type: ${commit_type}"
 
   if [ -n "${commit_scope}" ]; then
     message="${commit_type}(${commit_scope}): ${commit_message}"
@@ -56,6 +72,8 @@ main() {
   if [ -n "$commit_all" ]; then
     git add .
     git commit -am "${message}" && true
+  else
+    echo "${message}"
   fi
 
   if [ -n "$push" ]; then
--- src/selector.sh
+++ src/selector.sh
@@ -6,17 +6,23 @@ convcommit_selector() {
   local has_manual_input
   local input
   local key
+  local columns
+  local columns_width
+  local default_value
 
   convcommit_file="$1"
   stage="$2"
+  columns="${3:-1}"
+  columns_width="${4:-80}"
 
   echo "" >&2
-  echo "=========[ SELECT ${stage} ]=========" >&2
+  echo "==> Select ${stage}" >&2
 
   index=64
-  while read line; do
+  column=0
+  default_value=
+  while read -r line; do
     prefix=$(echo "${line}" | cut -d ':' -f 1)
-
     value=$(echo "${line}" | cut -d ':' -f 2)
 
     [ "${prefix}" != "${stage}" ] && continue
@@ -25,28 +31,49 @@ convcommit_selector() {
       continue
     fi
 
+    [ -z "${default_value}" ] && default_value="${value}"
+
     index=$((index + 1))
+    column=$((column + 1))
     letter=$(printf "\\$(printf '%03o' ${index})")
-    echo "[${letter}] ${value}" >&2
+
+    printf "[%s] %-${columns_width}s" "$letter" "$value" >&2
+    if [ "${column}" -eq "${columns}" ]; then
+      column=0
+      printf "\n" >&2
+    fi
   done < "${convcommit_file}"
 
   if [ "${index}" -gt 64 ]; then
     [ -n "${has_manual_input}" ] && echo "Press [space] for manual input" >&2
-    echo -n "Choose commit ${stage}: " >&2
+    echo -n "Choose commit ${stage} (default: ${default_value}): " >&2
     stty -icanon -echo
-    key=$(dd bs=1 count=1 2>/dev/null)
+    key=$(dd bs=1 count=1 2>/dev/null | tr '[:lower:]' '[:upper:]')
     stty icanon echo
     echo "" >&2
-    #echo "Hai premuto il tasto: $key" >&2
+    #echo "Pressed key: $key" >&2
+    index=64
+    while read -r line; do
+      prefix=$(echo "${line}" | cut -d ':' -f 1)
+      value=$(echo "${line}" | cut -d ':' -f 2)
+      [ "${prefix}" != "${stage}" ] && continue
+      [ "${value}" = "_" ] && continue
+      [ -z "${input}" ] && input="${value}"
+      index=$((index + 1))
+      letter=$(printf "\\$(printf '%03o' ${index})")
+      [ "${key}" = "${letter}" ] && input="${value}"
+    done < "${convcommit_file}"
   elif [ -n "${has_manual_input}" ]; then
     key=" "
   fi
 
   if [ "${key}" = " " ]; then
     echo -n "Manually type a ${stage}: " >&2
-    read input
+    read -r input
     echo "${stage}:${input}" >> "${convcommit_file}"
   fi
+
+  #echo "Your input is: ${input}" >&2
 
   echo "${input}"
 }
